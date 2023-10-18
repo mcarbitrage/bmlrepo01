8cc4971e5bffcab9b4c6fe809c002ddc4f539d66
mcarbitrage <mcarbitrage@users.noreply.github.com>
2023-10-18 17:07:33 +0100

    added safety header

--- wip-helloWorld.ps1
+++ wip-helloWorld.ps1
@@ -1,4 +1,12 @@
 # "wip-helloWorld.ps1"
+
+# Ask for confirmation before running the script
+$confirmation = Read-Host "Are you sure you want to run this script? (yes/no)"
+if ($confirmation -ne 'yes') {
+    Write-Host "Script execution cancelled."
+    exit
+}
+
 $hw = {
     Clear-Host
     "Hello World"
